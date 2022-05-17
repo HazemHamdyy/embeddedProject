1dd9f630aa93944fb133326cd16e41a145d8d4ae
SalmaEasa <1900117@eng.asu.edu.eg>
2022-05-17 20:45:58 +0200

    modifying

---
+++ case b.d
@@ -0,0 +1,8 @@
+.\case b.o: Case B.c
+.\case b.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\case b.o: tm4c123gh6pm.h
+.\case b.o: lcd.h
+.\case b.o: Keypad.h
+.\case b.o: systick.h
+.\case b.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\case b.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
